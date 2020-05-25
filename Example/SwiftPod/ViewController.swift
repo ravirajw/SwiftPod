fcf0cecb3b4ad68a60bffdb36f4f30461dd919f3
Raviraj <raviraj@sphinx-solution.com>
2020-05-25 14:11:43 +0530

    Initial commit

---
+++ Example/SwiftPod/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftPod
+//
+//  Created by w.raviraj@yahoo.com on 05/25/2020.
+//  Copyright (c) 2020 w.raviraj@yahoo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
